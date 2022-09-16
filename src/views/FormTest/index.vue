77c3a7d20a22e8557eb67627e51e15003761ca87
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-09-16 18:58:16 -0300

    example nested component with v-model

---
+++ src/views/FormTest/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <form>
+        <input name="name" v-model="name">
+        <input name="email" v-model="email">
+        <mailing-address v-model="address" />
+    </form>
+</template>
+<script>
+    import MailingAddress from './Address.vue';
+    export default {
+        components: { MailingAddress },
+        data() {
+            return {
+                name: '',
+                email: '',
+                address: {
+                    street: '',
+                    city: '',
+                    state: '',
+                    zip: ''
+                }
+            }
+        }
+    }
+</script>
