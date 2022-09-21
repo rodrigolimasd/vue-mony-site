b12b99a607ae81a443d267aeab03183086613ff9
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-09-21 18:26:47 -0300

    fix tooltip vs dropdown

--- src/views/Plan/index.vue
+++ src/views/Plan/index.vue
@@ -52,6 +52,7 @@
 </template>
 
 <script>
+import { Tooltip } from 'bootstrap/dist/js/bootstrap.esm.min.js'
 import { mapMutations } from 'vuex'
 import PlanItem from './PlanItem.vue'
 import PlanMonth from './PlanMonth.vue'
