11cf0daec42324fefbd1a095cdf216cc3508a7aa
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-10-10 19:50:10 -0300

    adjusts

--- src/views/Invoices/CreditCards/RowItem.vue
+++ src/views/Invoices/CreditCards/RowItem.vue
@@ -1,15 +1,15 @@
 <template>
      <div class="card card-body border card-plain border-radius-lg d-flex align-items-center flex-row">
-              <img class="w-10 me-3 mb-0" :src="getLogoCard(item.cardFlag)" alt="logo" />
-              <h6 class="mb-0">****&nbsp;&nbsp;&nbsp;****&nbsp;&nbsp;&nbsp;****&nbsp;&nbsp;&nbsp;{{item.finalNumberCard}}</h6>
-              <i
+            <img class="w-10 me-3 mb-0" :src="getLogoCard(item.cardFlag)" alt="logo" />
+            <h6 class="mb-0">****&nbsp;&nbsp;&nbsp;****&nbsp;&nbsp;&nbsp;****&nbsp;&nbsp;&nbsp;{{item.finalNumberCard}}</h6>
+            <i
                 class="fas fa-pencil-alt ms-auto text-dark cursor-pointer"
                 data-bs-toggle="tooltip"
                 data-bs-placement="top"
                 title="Edit">
             </i>
-              <span class="sr-only">Edit</span>
-            </div>
+            <span class="sr-only">Edit</span>
+      </div>
 </template>
 
 <script>
--- src/views/Invoices/CreditCards/index.vue
+++ src/views/Invoices/CreditCards/index.vue
@@ -16,7 +16,7 @@
       <div class="card-body p-3">
         <div class="row">
           <div class="col-md-6 mb-2" v-for="i in items" :key="i.finalNumbercard">
-            <CreditCardRowItem :item="i" />
+            <RowItem :item="i" />
           </div>
         </div>
       </div>
@@ -25,20 +25,94 @@
   
   <script>
   import ArgonButton from "@/components/ArgonButton.vue";
-  import CreditCardRowItem from "./CreditCardRowItem.vue";
+  import RowItem from "./RowItem.vue";
   
   export default {
     name: "credit-cards",
     components: {
       ArgonButton,
-      CreditCardRowItem
+      RowItem
     },
     data() {
       return {
           items: [
-            {
-                  cardFlag: "mastercard",
-                  finalNumberCard: "1234"
+           {
+                    name: "C6 Invoice",
+                    type: "creditcard",
+                    cardFlag: "mastercard",
+                    finalNumberCard: "1234",
+                    expirationDay: 15,
+                    closingDay: 6,
+                    limit: 35000.00,
+                    pointsPerValue: 0.47,
+                    invoices: [
+                        {
+                            month: 9,
+                            year: 2022,
+                            purchases: [
+                                {
+                                    value: 535.99,
+                                    totalValue: 535.99,
+                                    establishment: "new store eletronics",
+                                    description: "headphone",
+                                    category: "eletronic",
+                                    numberOfInstallments: 1,
+                                    installment: 1,
+                                    date: new Date(2022, 8, 10)
+                                },
+                                {
+                                    value: 117.99,
+                                    totalValue: 235.99,
+                                    establishment: "fine fashion",
+                                    description: "shirts",
+                                    category: "clothes",
+                                    numberOfInstallments: 2,
+                                    installment: 1,
+                                    date: new Date(2022, 8, 12)
+                                },
+                                {
+                                    value: 83.32,
+                                    totalvalue: 83.32,
+                                    establishment: "food delivery",
+                                    description: "lunch",
+                                    category: "food",
+                                    numberOfInstallments: 1,
+                                    installment: 1,
+                                    date: new Date(2022, 8, 15)
+                                },
+                                {
+                                    value: 95.25,
+                                    totalValue: 95.25,
+                                    establishment: "food delivery",
+                                    description: "dinner",
+                                    category: "food",
+                                    numberOfInstallments: 1,
+                                    installment: 1,
+                                    date: new Date(2022, 8, 18)
+                                },
+                                {
+                                    value: 103.35,
+                                    totalValue: 103.35,
+                                    establishment: "food delivery",
+                                    description: "dinner",
+                                    category: "food",
+                                    numberOfInstallments: 1,
+                                    installment: 1,
+                                    date: new Date(2022, 8, 20)
+                                },
+                                {
+                                    value: 325.08,
+                                    totalValue: 1300.35,
+                                    establishment: "lollapaloza event",
+                                    description: "entertainment",
+                                    category: "entertainment",
+                                    numberOfInstallments: 4,
+                                    installment: 1,
+                                    date: new Date(2022, 8, 22)
+                                }
+                            ]
+                        }
+                    ]
             },
             {
                   cardFlag: "visa",
