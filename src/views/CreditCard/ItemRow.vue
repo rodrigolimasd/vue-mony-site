8fb1003cbafbbd69546f7eeacbabf52037349c72
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-10-13 21:59:56 -0300

    adjusts list credit card

--- src/views/CreditCard/ItemRow.vue
+++ src/views/CreditCard/ItemRow.vue
@@ -1,17 +1,33 @@
 <template>
     <div class="card card-body border card-plain border-radius-lg d-flex align-items-center flex-row">
-           <img class="w-10 me-3 mb-0" :src="getLogoCard(item.cardFlag)" alt="logo" />
-           <h6 class="mb-0">****&nbsp;&nbsp;&nbsp;****&nbsp;&nbsp;&nbsp;****&nbsp;&nbsp;&nbsp;{{item.finalNumberCard}}</h6>
-           <router-link
-               tag="a"
-               :to="'/creditcard/'+item.id"
-               class="fas fa-pencil-alt ms-auto text-dark cursor-pointer"
-               data-bs-toggle="tooltip"
-               data-bs-placement="top"
-               title="Edit">
-           </router-link>
-           <!-- <router-link tag="li" to="/user/1" class="item-lista">User 1</router-link> -->
-           <span class="sr-only">Edit</span>
+        <img class="w-15 me-3 mb-0" :src="getLogoCard(item.cardFlag)" alt="logo" />
+                 <!-- <h6 class="mb-0">****&nbsp;&nbsp;&nbsp;****&nbsp;&nbsp;&nbsp;****&nbsp;&nbsp;&nbsp;{{item.finalNumberCard}}</h6> -->
+                 <h6 class="mb-0 d-inline">{{item.name}}</h6>
+                <div class="ms-auto">
+                    <router-link
+                    tag="a"
+                    :to="'/creditcard/'+item.id"
+                    class="fas fa-pencil-alt mx-1 text-dark cursor-pointer"
+                    data-bs-toggle="tooltip"
+                    data-bs-placement="top"
+                    title="Edit">
+                </router-link>
+                <a
+                    tag="a"
+                    class="fas fa-file mx-1 text-dark cursor-pointer"
+                    data-bs-toggle="tooltip"
+                    data-bs-placement="top"
+                    title="Invoice">
+                </a>
+                <a
+                    tag="a"
+                    :to="'/creditcard/'+item.id"
+                    class="fas fa-trash mx-1 text-dark cursor-pointer"
+                    data-bs-toggle="tooltip"
+                    data-bs-placement="top"
+                    title="Invoice">
+                </a>    
+                </div>
      </div>
 </template>
 
