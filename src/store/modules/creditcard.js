7dff8ee8370a6467d46dae329332a34ed37c18bb
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-10-13 21:08:21 -0300

    use localstorage to save creditcards

--- src/store/modules/creditcard.js
+++ src/store/modules/creditcard.js
@@ -1,11 +1,23 @@
+const saveStorageJson = (state) => {
+    localStorage.setItem('mony-creditcards', JSON.stringify(state.creditCards))
+}
+
+const recoveryStorageJson = () => {
+    let array = JSON.parse(localStorage.getItem('mony-creditcards'));
+    if(Array.isArray(array)) {
+      return array
+    }
+    return []
+}
 
 export default {
     state: {
-        creditCards: []
+        creditCards: recoveryStorageJson()
     },
     mutations: {
         addCreditCard(state, payload) {
             state.creditCards.push(payload)
+            saveStorageJson(state)
         }
     }
 }
