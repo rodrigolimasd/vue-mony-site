bcb21077dd115010e7224772166e4e770a487f27
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-10-21 16:59:27 -0300

    feat: new layout invoices

--- src/views/CreditCard/Invoice/index.vue
+++ src/views/CreditCard/Invoice/index.vue
@@ -4,38 +4,7 @@
             <div class="col-lg-8">
                 <div class="row mt-4">
                     <div class="col-12">
-                        <div class="card pb-4">
-                            <div class="card-header pb-0 p-3">
-                                <div class="row">
-                                    <div class="col-4 d-flex align-items-center">
-                                        <h4 class="mb-0">Invoices</h4>
-                                    </div>
-                                    <div class="col-8 text-end">
-                                        <argon-button color="default"
-                                                @click="back"
-                                                variant="gradient" class="me-2">
-                                            <i class="fas fa-arrow-left me-2"></i>
-                                            Back
-                                        </argon-button>
-                                        <argon-button
-                                            color="dark" variant="gradient">
-                                            <i class="fas fa-plus me-2"></i>
-                                            Add New Invoice
-                                        </argon-button>
-                                        <argon-button class="mx-2"
-                                            color="success" variant="gradient">
-                                            <i class="fas fa-check"></i>
-                                            Save
-                                        </argon-button>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="card-body p3 pb-0 mb-0">
-                                <invoice :invoicesByYear="invoicesByYear" />
-                            </div>
-                            <div class="card-footer pt-4">
-                            </div>
-                        </div>
+                        <card-invoice></card-invoice>
                     </div>
                     <div class="col-xl-6">
                         <div class="row">
@@ -57,16 +26,14 @@
 
 <script>
 
-import ArgonButton from "@/components/ArgonButton.vue"
 import CreditCard from "@/components/cards/CreditCard.vue"
-import Invoice from './Invoice.vue'
+import CardInvoice from './CardInvoice.vue'
 
 export default {
     props:['id'],
     components: {
-        ArgonButton,
         CreditCard,
-        Invoice
+        CardInvoice
     },
     data() {
         return {
