c516a117b781cb18350593a8d16703424a071b78
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-11-03 20:31:21 -0300

    asjuts

--- src/views/CreditCard/index.vue
+++ src/views/CreditCard/index.vue
@@ -8,7 +8,7 @@
                         <div class="card-header pb-0 p-3">
                             <div class="row">
                             <div class="col-6 d-flex align-items-center">
-                                <h6 class="mb-0">Credit Cards</h6>
+                                <h4 class="mb-0">Credit Cards</h4>
                             </div>
                             <div class="col-6 text-end">
                                 <argon-button color="dark" variant="gradient" @click="add">
